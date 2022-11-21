5fab074f30f3d0b41fff026cf2636f5be24b3e8c
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-22 01:18:30 +0900

    TEAM-455 디스커버리 클라이언트 설정

--- src/main/java/com/tobias/cartService/BasketServiceApplication.java
+++ src/main/java/com/tobias/cartService/BasketServiceApplication.java
@@ -2,8 +2,10 @@ package com.tobias.cartService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BasketServiceApplication {
 
     public static void main(String[] args) {
